192a458caad3a9fe57740042be6b60034a6dcc65
JordanVScher <jordanvscher@hotmail.com>
2019-02-28 17:53:41 -0300

    Log

--- app/utils/flow.js
+++ app/utils/flow.js
@@ -2,14 +2,14 @@ module.exports = {
   avatarImage: 'https://www.telegraph.co.uk/content/dam/news/2016/09/08/107667228_beech-tree-NEWS_trans_NvBQzQNjv4BqplGOf-dgG3z4gg9owgQTXEmhb5tXCQRHAvHRWfzHzHk.jpg?imwidth=450',
   getStarted: 'oi sou o bot',
   greetings: {
-    text1: 'Hi, im a chatbot',
-    text2: 'I do this and that',
+    text1: 'oi, eu sou o bot',
+    text2: 'faço isso e aquilo',
   },
   mainMenu: {
-    text1: 'on mainMenu',
+    text1: 'No menu',
   },
   issueText: {
-    success: 'Thank you for your message. We will answer you soon',
-    failure: 'I failed at saving the message',
+    success: 'obg pela mensagem, já te respondo',
+    failure: 'Não consegui salvar sua msg, deu erro',
   },
 };
